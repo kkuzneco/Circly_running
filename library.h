305f4e44bdf3df6e6b8a39a7352ad8645e665f8a
Darya Gorbunova <gorbunov@cs.karelia.ru>
2018-05-08 13:53:13 +0300

    библиотека

--- library.h
+++ library.h
@@ -6,12 +6,62 @@
 #include <QtGui>
 //#include <QDeclarativeContext>
 #include <QObject>
+#include <list>
 
-class Library : public QObject
+class Llibrary : public QObject
 {
     Q_OBJECT
+  struct Point
+       {
+           int x, y;
+        };
+//  class Character
+//  {
+//  public:
+//          /* Get states */
+//          int getCurLives();
+//          unsigned int getMaxLives();
+//          unsigned int getDamage();
+
+
+//          /* Character gets damage and health */
+
+//          /* Returns true, when character is alive
+//           * Returns false, when character is dead
+//           */
+//          //virtual bool takeDamage(unsigned int amountOfDamage);
+
+
+
+//  private:
+//          unsigned int maxLives;
+//          /* Current health */
+//          int curLives;
+
+//  protected:
+//          /*Setting*/
+//          void setCurHealth(int amountOfCurHealth);
+//          void setMaxHealth(unsigned int amountOfMaxHealth);
+//          void setWeapon(Weapon* weapon);
+//  };
+//  class Hero: public Character
+//  {
+//  public:
+//      Hero(unsigned int amountOfMaxHealth, unsigned int maxLevel);
+
+
+//      unsigned int getGold();
+//      bool takeGold(int amountOfGold);
+//     unsigned int getHeroicDamage();
+////    Level(int heroX, int heroY, unsigned int lives);
+
+//// Enemy* addEnemy(int x, int y);
+
+////       list <EnemyObject*> getListOfMonsters();
+
+
 public:
-    explicit Library(QObject *parent = nullptr);
+    explicit Llibrary(QObject *parent = nullptr);
 
 signals:
 
