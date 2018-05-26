ba8600f6ad0fb33f2d17c5ba25151ddf585d20fa
Kseniya Kuznetsova <1k9k9k9@mail.ru>
2018-05-26 11:06:23 +0300

    папка external

--- tests/tests.h
+++ tests/tests.h
@@ -1,1 +1,16 @@
- 
+#ifndef CHECK1_H
+#define CHECK1_H
+
+#include <gtest/gtest.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include "checking1.h"
+
+
+TEST(checkingblocks, test1){
+  CheckingPosition c1;
+  c1.check_pos(100,100);
+  SUCCEED();
+}
+#endif
